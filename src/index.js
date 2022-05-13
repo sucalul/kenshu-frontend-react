2b2d0ba518f91200bfba0c4faa3d02a4c332f4a0
yuya.miyazaki <cheeeese4869@gmail.com>
2022-05-13 18:08:39 +0900

    lesson 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(<App subject="React"/>);
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
+
+createRoot(document.getElementById("root")).render(<App tasks={DATA} />);
